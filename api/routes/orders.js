1a90ba28a3d87ddc9bc056df2394e0fed63474db
coos <iceliang@gmail.com>
2019-05-07 21:46:41 +1200

    log and error

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -13,6 +13,7 @@ router.post("/", (req, res, next) => {
 	});
 });
 
+//  Handle incoming GET Requests to /orders
 router.get("/:orderId", (req, res, next) => {
 	res.status(200).json({
 		message: "Order details.",
