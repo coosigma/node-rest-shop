944d46697ff956359802fab7cee9d1ec5a7eef9f
coos <iceliang@gmail.com>
2019-06-04 21:16:33 +1200

    generate token

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -42,6 +43,49 @@ router.post("/signup", (req, res, next) => {
 					}
 				});
 			}
+		});
+});
+router.post("/login", (req, res, next) => {
+	User.findOne({ email: req.body.email })
+		.exec()
+		.then(user => {
+			if (!user) {
+				return res.status(401).json({
+					message: "Auth failed"
+				});
+			}
+			bcrypt.compare(req.body.password, user.password, (err, result) => {
+				if (err) {
+					return res.status(401).json({
+						message: "Auth failed"
+					});
+				}
+				if (result) {
+					const token = jwt.sign(
+						{
+							email: user.email,
+							userId: user._id
+						},
+						process.env.JWT_KEY,
+						{
+							expiresIn: "1h"
+						}
+					);
+					return res.status(200).json({
+						message: "Auth successful",
+						token: token
+					});
+				}
+				res.status(401).json({
+					message: "Auth failed"
+				});
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
 		});
 });
 
